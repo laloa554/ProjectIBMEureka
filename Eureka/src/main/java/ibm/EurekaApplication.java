4eba7e95ef6f5c595393143d3883d3619cbcd8f9
laloa554 <55073002+laloa554@users.noreply.github.com>
2021-10-17 14:29:03 -0500

    Add files via upload

---
+++ Eureka/src/main/java/ibm/EurekaApplication.java
@@ -0,0 +1,18 @@
+package ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@EnableEurekaServer
+//@EnableFeignClients
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
